2d35420c8fa5965f073ff1c8613be24a2005695d
Austin Au-Yeung <austinay96@gmail.com>
2023-09-17 22:44:24 -0500

    Present preferences to authenticated user

--- frontend/src/containers/Home.tsx
+++ frontend/src/containers/Home.tsx
@@ -1,10 +1,41 @@
 import "./Home.css";
 import { useAppContext } from "../lib/contextLib";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { API } from "aws-amplify";
+import { Accordion, Form, Button } from "react-bootstrap";
+import * as AWS from 'aws-sdk';
+import { toast } from "react-toastify";
+import LoaderButton from "../components/LoaderButton";
 
 export default function Home() {
+    interface Feed {
+        feedName: string;
+        category: string;
+        subfeeds: Record<string, string>;
+    }
+
+    interface SubfeedPreferences {
+        [subfeedName: string]: number;
+    }
+
+    interface Preferences {
+        feedEnabled: number,
+        feeds: {
+            [feedName: string]: SubfeedPreferences
+        },
+        fetchTime: number,
+        kindleEmail: string,
+    }
+
     const { isAuthenticated } = useAppContext();
+    const [metadata, setMetadata] = useState<Feed[]>([]);
+    const [preferences, setPreferences] = useState<Preferences>({
+        feedEnabled: 0,
+        feeds: {},
+        fetchTime: 6,
+        kindleEmail: ''
+    });
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         async function onLoad() {
@@ -15,16 +46,84 @@ export default function Home() {
             try {
                 const [metadataResponse, preferencesResponse] = await Promise.all([
                     API.get("RestApi", "metadata", {}),
-                    API.get("RestApi", "preferences", {}),
+                    API.get("RestApi", "preferences", {})
                 ]);
-                console.log(metadataResponse);
-                console.log(preferencesResponse);
+                const fetchedMetadata = metadataResponse.data.Items;
+                const fetchedPreferences = AWS.DynamoDB.Converter.unmarshall(preferencesResponse.data.Item);
+                const updatedFeeds = { ...fetchedPreferences.feeds };
+
+                fetchedMetadata.forEach((feed: Feed) => {
+                    if (!(feed.feedName in updatedFeeds)) {
+                        updatedFeeds[feed.feedName] = {}
+                    }
+                    Object.keys(feed.subfeeds).forEach(subfeed => {
+                        if (!(subfeed in updatedFeeds[feed.feedName])) {
+                            updatedFeeds[feed.feedName][subfeed] = 0
+                        }
+                    })
+                });
+
+                const updatedPreferences = {
+                    ...fetchedPreferences,
+                    feeds: updatedFeeds
+                } as Preferences;
+                console.log(updatedPreferences)
+                setMetadata(fetchedMetadata);
+                setPreferences(updatedPreferences);
             } catch (error) {
                 console.log(error);
             }
         }
         onLoad();
-    })
+    }, [])
+
+    const categories = metadata.reduce((acc, feed) => {
+        if (!acc[feed.category]) {
+            acc[feed.category] = [];
+        }
+        acc[feed.category].push({
+            feedName: feed.feedName,
+            subfeeds: feed.subfeeds
+        });
+        return acc;
+    }, {} as Record<string, { feedName: string, subfeeds: Record<string, string> }[]>)
+
+    function handleCheckboxChange(feedName: string, subfeed: string) {
+        setPreferences(prevPreferences => {
+            const updatedFeeds = { ...prevPreferences.feeds[feedName] }
+            for (const key in updatedFeeds) {
+                updatedFeeds[key] = 0
+            }
+            updatedFeeds[subfeed] = prevPreferences.feeds[feedName][subfeed] === 1 ? 0 : 1;
+
+            const newPreferences = {
+                ...prevPreferences,
+                feeds: {
+                    ...prevPreferences.feeds,
+                    [feedName]: updatedFeeds
+                }
+            }
+            console.log(newPreferences)
+            return newPreferences;
+        });
+    }
+
+    async function updateData(event: React.FormEvent<HTMLFormElement>, preferences: Preferences) {
+        event.preventDefault();
+        setIsLoading(true);
+
+        try {
+            console.log(preferences)
+            const response = await API.put("RestApi", "preferences", {
+                body: preferences
+            })
+            console.log(response);
+            toast("Preferences saved.", { toastId: "saved" })
+        } catch (error) {
+            console.log(error);
+        }
+        setIsLoading(false);
+    }
 
     function renderLander() {
         return (
@@ -44,6 +143,8 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="bottom-container">
+                    <a>Terms of Service</a>
+                    <a>Privacy Policy</a>
                     <a>About</a>
                     <a>FAQs</a>
                     <a>GitHub</a>
@@ -52,17 +153,59 @@ export default function Home() {
         );
     }
 
-    function renderFeeds() {
+    function renderFeeds(categories: Record<string, { feedName: string, subfeeds: Record<string, string> }[]>) {
         return (
             <div className="Feeds">
-                <h1>Feeds</h1>
+                <h1>Feed Preferences</h1>
+                {Object.keys(preferences.feeds).length > 1 && (
+                    <Form onSubmit={(event) => updateData(event, preferences)}>
+                        <Accordion alwaysOpen>
+                            {Object.entries(categories).map(([category, feeds], index) => (
+                                <Accordion.Item key={category} eventKey={index.toString()}>
+                                    <Accordion.Header>{category}</Accordion.Header>
+                                    <Accordion.Body>
+                                        {feeds.map(feed => {
+                                            const { feedName } = feed;
+                                            return (
+                                                <div key={feedName} className="AccordionFeed">
+                                                    {feedName}
+                                                    {Object.keys(feed.subfeeds).map(subfeed => (
+                                                        <Form.Check
+                                                            className=".custom-border-radius"
+                                                            type='checkbox'
+                                                            id={subfeed}
+                                                            label={subfeed}
+                                                            key={subfeed}
+                                                            checked={preferences.feeds[feed.feedName][subfeed] === 1}
+                                                            onChange={() => handleCheckboxChange(feed.feedName, subfeed)}
+                                                        />
+                                                    ))}
+                                                </div>
+                                            )
+                                        })}
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            ))}
+                        </Accordion>
+                        <LoaderButton
+                            // size="lg"
+                            type="submit"
+                            variant="dark"
+                            isLoading={isLoading}
+                            className="mt-3 float-end"
+                        >
+                            {!isLoading && "Save"}
+                        </LoaderButton>
+                        {/* <Button className="mt-3 float-end" as="input" type="submit" value="Save" variant="dark" onClick={() => updateData(preferences)} />{' '} */}
+                    </Form>
+                )}
             </div>
         )
     }
 
     return (
         <div className="Home">
-            {isAuthenticated ? renderFeeds() : renderLander()}
+            {isAuthenticated ? renderFeeds(categories) : renderLander()}
         </div>
     );
 
