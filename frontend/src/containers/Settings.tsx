1867c47b4b9e2bede7e9b34ce216380fa389e405
Austin Au-Yeung <austinay96@gmail.com>
2023-09-19 01:58:40 -0500

    Format settings route

--- frontend/src/containers/Settings.tsx
+++ frontend/src/containers/Settings.tsx
@@ -1,9 +1,21 @@
 import { Auth } from "aws-amplify";
 import "./Settings.css";
 import { useState, useEffect } from "react";
+import { Form, InputGroup } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import { useAppContext } from "../lib/contextLib";
+import { handlePrefChange } from "../lib/hooksLib";
+import updateData from "../lib/putPreferences"
+import { useNavigate } from "react-router-dom";
 
 export default function Settings() {
+    const { preferences, setPreferences } = useAppContext();
+    const [tempPreferences, setTempPreferences] = useState(preferences);
     const [email, setEmail] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const nav = useNavigate();
+
+    console.log(preferences)
 
     useEffect(() => {
         async function getUser() {
@@ -13,6 +25,7 @@ export default function Settings() {
                 setEmail(user.attributes.email);
             } catch (error) {
                 console.log(error);
+                nav("/");
             }
         }
         getUser();
@@ -20,23 +33,57 @@ export default function Settings() {
 
     return (
         <div className="Settings">
-            <div className="SettingsElement">
+            <div className="mb-3">
                 <h2>Signed in as</h2>
                 <h3>{email}</h3>
             </div>
-            <div className="SettingsElement">
-                <h2>Kindle E-mail</h2>
-                <h3>test</h3>
-            </div>
-            <div className="SettingsElement">
+            <Form onSubmit={(event) => updateData(event, tempPreferences, setPreferences, setIsLoading)} noValidate>
                 <h2>Feed Delivery</h2>
-                <h3>Enabled</h3>
-                <h3>Time</h3>
-            </div>
-            <div className="SettingsElement">
-                <h2>Delete Account</h2>
-                <h3>test</h3>
-            </div>
+                <Form.Check
+                    type="checkbox"
+                    id="feedEnabled"
+                    checked={Boolean(tempPreferences.feedEnabled)}
+                    label={`${tempPreferences.feedEnabled ? "Enabled" : "Disabled"}`}
+                    onChange={(event) => handlePrefChange(event, setTempPreferences)}
+                    className="mb-3 FeedEnabled"
+                />
+
+                <h2>Your Kindle E-mail</h2>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                        size="lg"
+                        type="email"
+                        value={tempPreferences.kindleEmail}
+                        id="kindleEmail"
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => handlePrefChange(event, setTempPreferences)}
+                    />
+                    <InputGroup.Text id="kindleEmail">@kindle.com</InputGroup.Text>
+                </InputGroup>
+
+                <h2>Delivery Time (EST)</h2>
+                <Form.Select className="select-wrapper" id="fetchTime" value={tempPreferences.fetchTime} onChange={(event) => handlePrefChange(event, setTempPreferences)}>
+                    {[...Array(24).keys()].map(hour => {
+                        return (
+                            <option key={`${hour}`} className="dropdown" value={hour}>
+                                {hour === 0 ? "12 AM" : (
+                                    hour < 12 ? `${hour} AM` : (
+                                        hour === 12 ? "12 PM" : `${hour - 12} PM`
+                                    )
+                                )}
+                            </option>
+                        )
+                    })}
+                </Form.Select>
+                <LoaderButton
+                    type="submit"
+                    variant="dark"
+                    isLoading={isLoading}
+                    className="mt-3 mb-5 ms-auto"
+                >
+                    {!isLoading && "Save"}
+                </LoaderButton>
+            </Form>
+            <h2 className="mt-5 text-center">Delete Account</h2>
         </div>
     );
 }
