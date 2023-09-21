f11d31dc50d649435ffa1c3dd5e3ab2709256eda
Austin Au-Yeung <austinay96@gmail.com>
2023-09-21 01:02:09 -0500

    Adjust signup page styling

--- frontend/src/containers/Signup.tsx
+++ frontend/src/containers/Signup.tsx
@@ -50,54 +50,52 @@ export default function Signup() {
         return (
             <MotionDiv variant="fadeIn" delay={0.1} className="Signup">
                 <h1>Welcome.</h1>
-                <div>
-                    <Form onSubmit={handleSubmit}>
-                        <Stack gap={1}>
-                            <Form.Group controlId="email">
-                                {/* <Form.Label>Email</Form.Label> */}
-                                <Form.Control
-                                    size="lg"
-                                    autoFocus
-                                    type="email"
-                                    value={fields.email}
-                                    onChange={handleFieldChange}
-                                    placeholder="E-mail"
-                                />
-                            </Form.Group>
-                            <Form.Group controlId="password">
-                                {/* <Form.Label>Password</Form.Label> */}
-                                <Form.Control
-                                    size="lg"
-                                    type="password"
-                                    value={fields.password}
-                                    onChange={handleFieldChange}
-                                    placeholder="Password"
-                                />
-                            </Form.Group>
-                            <Form.Group controlId="confirmPassword">
-                                {/* <Form.Label>Confirm Password</Form.Label> */}
-                                <Form.Control
-                                    size="lg"
-                                    type="password"
-                                    value={fields.confirmPassword}
-                                    onChange={handleFieldChange}
-                                    placeholder="Confirm Password"
-                                />
-                            </Form.Group>
-                            {!validPassword(fields.password) && fields.password.length > 0 && <Form.Text muted>Password must have a minimum length of 8 and contain a number, a special character, an uppercase letter, and a lowercase letter.</Form.Text>}
-                            {(validPassword(fields.password) && fields.password.length > 0 && fields.password !== fields.confirmPassword) && <Form.Text muted>Passwords must match.</Form.Text>}
-                            <LoaderButton
+                <Form onSubmit={handleSubmit}>
+                    <Stack gap={1}>
+                        <Form.Group controlId="email">
+                            {/* <Form.Label>Email</Form.Label> */}
+                            <Form.Control
                                 size="lg"
-                                type="submit"
-                                variant="dark"
-                                isLoading={isLoading}
-                                disabled={!validateForm(fields, 'signup')}
-                            >
-                                {!isLoading && "Signup"}
-                            </LoaderButton>
-                        </Stack>
-                    </Form>
-                </div>
+                                autoFocus
+                                type="email"
+                                value={fields.email}
+                                onChange={handleFieldChange}
+                                placeholder="E-mail"
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            {/* <Form.Label>Password</Form.Label> */}
+                            <Form.Control
+                                size="lg"
+                                type="password"
+                                value={fields.password}
+                                onChange={handleFieldChange}
+                                placeholder="Password"
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="confirmPassword">
+                            {/* <Form.Label>Confirm Password</Form.Label> */}
+                            <Form.Control
+                                size="lg"
+                                type="password"
+                                value={fields.confirmPassword}
+                                onChange={handleFieldChange}
+                                placeholder="Confirm Password"
+                            />
+                        </Form.Group>
+                        {!validPassword(fields.password) && fields.password.length > 0 && <Form.Text muted>Password must have a minimum length of 8 and contain a number, a special character, an uppercase letter, and a lowercase letter.</Form.Text>}
+                        {(validPassword(fields.password) && fields.password.length > 0 && fields.password !== fields.confirmPassword) && <Form.Text muted>Passwords must match.</Form.Text>}
+                        <LoaderButton
+                            size="lg"
+                            type="submit"
+                            variant="dark"
+                            isLoading={isLoading}
+                            disabled={!validateForm(fields, 'signup')}
+                        >
+                            {!isLoading && "Signup"}
+                        </LoaderButton>
+                    </Stack>
+                </Form>
             </MotionDiv>
         );
     }
